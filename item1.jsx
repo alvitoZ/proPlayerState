e6a5f1d93cdd89f182d49ce58af8658d13480e04
alvitoZ <107417445+alvitoZ@users.noreply.github.com>
2023-01-02 01:25:36 +0700

    Update item1.jsx

--- item1.jsx
+++ item1.jsx
@@ -1,1 +1,28 @@
 
+import React, { Component, Fragment } from "react";
+import Item2 from "./Item2";
+
+class Item1 extends Component {
+  state = {
+    value: 0,
+  };
+
+  handleChange(newValue) {
+    this.setState({
+      value: newValue,
+    });
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <p>item1 {this.state.value}</p>
+        <Item2
+          itval={this.state.value}
+          onHandleChange={(newValue) => this.handleChange(newValue)}
+        />
+      </Fragment>
+    );
+  }
+}
+export default Item1;
