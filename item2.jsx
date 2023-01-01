d9260bd0fd9dffcd2cb91630e22e8b5285a5eb44
alvitoZ <107417445+alvitoZ@users.noreply.github.com>
2023-01-02 01:25:51 +0700

    Update item2.jsx

--- item2.jsx
+++ item2.jsx
@@ -1,1 +1,42 @@
 
+import React, { Component } from "react";
+
+class Item2 extends Component {
+  // state = {
+  //   value: 0,
+  // };
+
+  // handleChange = () => {
+  //   this.props.onHandleChange(this.state.value + 1);
+  // };
+
+  // handlePlus = () => {
+  //   this.setState({
+  //     value: this.state.value + 1,
+  //   });
+
+  //   this.handleChange();
+  // };
+
+  handleChange() {
+    this.props.onHandleChange(this.props.itval + 1);
+  }
+
+  // handlePlus = () => {
+  //   this.setState(
+  //     {
+  //       value: this.state.value + 1,
+  //     },
+  //     () => this.handleChange(this.state.value)
+  //   );
+  // };
+  render() {
+    return (
+      <div>
+        <p>item2: {this.props.itval}</p>
+        <h1 onClick={() => this.handleChange()}>TAMBAH</h1>
+      </div>
+    );
+  }
+}
+export default Item2;
